53c5aa593e53736c925efcdb46cac1bc8ffbd917
霧島ひなた <mastodon@mail.kirishima.cloud>
2020-01-23 05:03:20 +0900

    Updates

---
+++ precache-manifest.2d3fcf8656ec970b0eac9f1fe31342f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe17c6490b366bdde1a8df9d9985963",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "cfde019ad8c537d97c61",
+    "url": "/blog/static/css/main.fdd8c1f8.chunk.css"
+  },
+  {
+    "revision": "4d61c91ad4290d7eeab5",
+    "url": "/blog/static/js/2.32cc478d.chunk.js"
+  },
+  {
+    "revision": "3e6887b9ed6d8bee1d899d2295e5baa1",
+    "url": "/blog/static/js/2.32cc478d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfde019ad8c537d97c61",
+    "url": "/blog/static/js/main.e2d89435.chunk.js"
+  },
+  {
+    "revision": "bd76032da4b543995fd4",
+    "url": "/blog/static/js/runtime-main.288467b5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.ff56a98161a4b24c93256d2648d9e3f7.js"
+  "/blog/precache-manifest.2d3fcf8656ec970b0eac9f1fe31342f6.js"
 );
 
 self.addEventListener('message', (event) => {
