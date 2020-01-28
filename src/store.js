b707e481bd30d2b24089e7bf9e7bfca93c8069c0
霧島ひなた <mastodon@mail.kirishima.cloud>
2020-01-28 13:46:07 +0900

    remove console.log

--- src/store.js
+++ src/store.js
@@ -3,19 +3,14 @@ import {createStore} from "redux";
 export const reducers = (state = "top", action) => {
   switch (action.type) {
     case 'TOP':
-      console.log("top動きました")
       return state = "top"
     case 'ABOUT':
-      console.log("about動きました")
       return state = "about"
     case 'PRODUCT':
-      console.log("product動きました")
       return state = "product"
     case 'BLOG':
-      console.log("blog動きました")
       return state = "blog"
     case 'CONTACT':
-      console.log("contact動きました")
       return state = "contact"
     default:
       return state
